44af4cd174db4f6c8400b100bd43cd3a6442fc0e
adhisaputra <adhisaputra11@gmail.com>
2019-05-21 11:43:36 +0800

    landing 1

--- php/landing1.php
+++ php/landing1.php
@@ -1,18 +1,24 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="../vendor/holies/css/landing.css">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap-modified.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../vendor/holies/css/navbar.css">
+    <!-- Required meta tags -->
+    
+    <link rel="stylesheet" href="../vendor/holies/css/landing1.css">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
 
-    <title>Sandhika Galih & Angga Risky</title>
+    <title>HOLIES | House of Lifestock</title>
   </head>
   <body>
 
@@ -27,10 +33,9 @@
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav ml-auto">
             <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Pricing</a>
             <a class="nav-item nav-link" href="#">Features</a>
             <a class="nav-item nav-link" href="#">About</a>
-            <a class="nav-item nav-link btn btn-primary text-white tombol" href="#">Join Us</a>
+            <a class="nav-item nav-link btn btn-primary text-white tombol" href="login.php">LOGIN</a>
           </div>
         </div>
       </div>
@@ -40,8 +45,8 @@
     <!-- Jumbotron -->
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
-        <h1 class="display-4">Get work done <span>faster</span> <br>and <span>better</span> with us</h1>
-        <a href="#" class="btn btn-primary tombol">Our Work</a>
+        <h1 class="display-4">Gain Your <span>Livestock</span> <br>Gain Your <span>Life</span></h1>
+        <a href="register.php" class="btnjoin">JOIN US</a>
       </div>
     </div>
     <!-- akhir Jumbotron -->
@@ -56,18 +61,18 @@
           <div class="row">
             <div class="col-sm">
               <img src="img/employee.png" alt="Employee" class="img-fluid float-left">
-              <h4>24 Hours</h4>
-              <p>Lorem ipsum dolor sit amet.</p>
+              <h4>Sell</h4>
+              <p>Jual Hasil Peternakan</p>
             </div>
             <div class="col-lg">
               <img src="img/hires.png" alt="HiRes" class="img-fluid float-left">
-              <h4>High-Res</h4>
-              <p>Lorem ipsum dolor sit amet.</p>
+              <h4>Buy</h4>
+              <p>Beli Segala Keperluan Peternakan</p>
             </div>
             <div class="col-lg">
               <img src="img/security.png" alt="Security" class="img-fluid float-left">
-              <h4>Security</h4>
-              <p>Lorem ipsum dolor sit amet.</p>
+              <h4>Ask</h4>
+              <p>Tanya Kepada Ahlinya di Holies</p>
             </div>
           </div>
         </div>
@@ -78,13 +83,13 @@
       <!-- Workingspace -->
       <div class="row workingspace">
         <div class="col-lg-6">
-          <img src="img/workingspace.png" alt="Working Space" class="img-fluid">
+          <img src="../img/logo.png" alt="Working Space" class="img-fluid logo">
         </div>
         <div class="col-lg-5">
-          <h2>You <span>Work</span> Like at <span>Home</span></h2>
+          <h2>You Can <span>Work</span> Every <span>Where</span></h2>
           <p>Bekerja dengan suasana hati yang lebih asik, menyenangkan
             dan mempelajari hal baru setiap harinya.</p>
-          <a href="#" class="btn btn-primary tombol">Gallery</a>
+          <a href="#" class="btn btn-primary tombol">HOW WE WORK</a>
         </div>
       </div>
       <!-- akhir Workingspace -->
@@ -94,21 +99,18 @@
       <section class="testimonial">
         <div class="row justify-content-center">
           <div class="col-lg-8">
-            <p>"Bekerja dengan suasana hati yang lebih asik, menyenangkan
-              dan mempelajari hal baru setiap harinya."</p>
+            <p>"Bergabung dengan holies memberikan saya banyak pengalaman berharga"</p>
           </div>
         </div>
         <div class="row justify-content-center">
           <div class="col-lg-6 justify-content-center d-flex">
-            <img src="img/img1.png" alt="Testimonial 1">
-            <img src="img/img2.png" alt="Testimonial 2" class="img-main">
-            <img src="img/img3.png" alt="Testimonial 3">
+            <img src="../img/mark.png" alt="Testimonial 2" class="img-main">
           </div>
         </div>
         <div class="row justify-content-center info-text">
           <div class="col-lg text-center">
-            <h5>Sunny Ye</h5>
-            <p>Designer</p>
+            <h5>Mark</h5>
+            <p>Seller</p>
           </div>
         </div>
       </section>
