4e104bee43508a70d1891265850615656d298da1
adhisaputra <adhisaputra11@gmail.com>
2019-05-29 01:18:21 +0800

    penambahan detail barang

---
+++ home/detail_barang.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>HOLIES - House of Livestock</title>
+
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap-modified.min.css">
+	<link rel="stylesheet" href="../vendor/holies/css/detailbarang.css">	
+
+</head>
+<body>
+	<?php include 'nav.php' ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-8">
+				<div class="shadow bg-white rounded mt-3 p-3 box1">
+					<img src="../img/telur.jpg" class="img-fluid img rounded float-left mr-3 mb-3" alt="Responsive image">
+					<p class="text-left float-left"><h3>TELUR AYAM | Baru Aja Keluar Kemaren</h3></p>
+					<p class="text-left float-left"><h3 class="text-success font-weight-bold">Rp. 9.000</h3></p>
+					<p class="text-secondary">Tersedia 100 Kg</p>
+					<p class="text-secondary">Masukkan jumlah yang diinginkan</p>
+					<button type="button" class="btn btn-primary btn-sm bg-success float-left">-</button>
+					<span class="border border-secondary float-left box2"><p class="text-secondary text-center">1</p></span>
+					<button type="button" class="btn btn-secondary btn-sm bg-success float-left">+</button>
+					<button type="button" class="btn btn-success btn-lg btn-block">BELI SEKARANG</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block">TAMBAH KE KERANJANG</button>
+				</div>
+				<div class="shadow bg-white rounded mt-3 mb-5 p-3 box3">
+					<div class="btn-group btn-group-toggle" data-toggle="buttons">
+						<label class="btn btn-success active">
+						    <input type="radio" name="options" id="option1" autocomplete="off" checked> Detail Barang
+						</label>
+					</div>
+					<table class="table table-borderless table-responsive">
+							    <tr>
+							    	<th scope="row">INFORMASI</th>
+							    	<td><i class="fa fa-shopping-cart"></i></td>
+							    	<td>Dibeli</td>
+							    	<td>:</td>
+							    	<td>36</td>
+							    </tr>
+							    <tr>
+							    	<th></th>
+							    	<td><i class="fa fa-heart"></i></td>
+							    	<td>Disukai</td>
+							    	<td>:</td>
+							    	<td>56</td>
+							    </tr>
+							    <tr>
+							    	<th></th>
+							    	<td><i class="fas fa-upload"></i></td>
+							    	<td>Diperbaharui</td>
+							    	<td>:</td>
+							    	<td>12 Mei 2019</td>
+							    </tr>
+					</table>
+					<div class="p-3">
+						<h4 class="font-weight-bold">Deskripsi</h4>
+						<p class="pl-5 text-justify">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="shadow bg-white rounded col-4 box3 mt-3 p-3">
+				<h3>Tentang Seller</h3>
+				<div class="text-center mt-3">
+					<img src="../img/mark.png" class="PP" alt="PP">
+				</div>
+				<h2 class="text-center mt-3">Siapa Saya</h2>
+				<div class="text-center mt-3"><i class="fas fa-map-marker-alt">  Kab. Tabanan</i></div>
+				<div class="text-center m-3"><i class="fas fa-calendar-week">  Bergabung : 12 Jan 2019</i></div>
+				<button type="button" class="btn btn-success btn-lg btn-block">IKUTI</button>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
