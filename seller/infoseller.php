ece551062ebbd74948045ad84d023af51333fe67
widiantariputri <ayutari16@gmail.com>
2019-05-29 17:35:25 +0800

    tambahan dari folder seller

---
+++ seller/infoseller.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOLIES - House of Livestock</title>
+
+	<!-- Bootstrap core CSS -->
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap-modified.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../vendor/holies/css/sherly.css">
+</head>
+<body>
+	<?php include '../home/nav.php' ?>
+	<div class="row m-3 justify-content-md-center">
+		<div class="shadow mb-3 bg-white rounded box2 p-3">
+			<div class="gambar-diri float-left">
+				<img src="../img/mark.png" class="rounded-circle" style="width: 100%; height: 100%;">
+			</div>
+			<div class="detail float-left ml-3">
+				<h3 class="pl-1">I Gde Made Hendra Pradiptha</h3>
+				<table class="table-responsive">
+					<tr>
+						<td><i class="fas fa-map-marker-alt text-success m-2"></i></td>
+						<td>Jalan Tukad Balian No 95 Denpasar</td>
+					</tr>
+					<tr>
+						<td><i class="far fa-envelope text-success m-2"></i></i></td>
+			      		<td>gdehendrapradiptha@gmail.com</td>
+					</tr>
+					<tr>
+						<td><i class="fa fa-user text-success m-2"></i></i></td>
+						<td>Bergabung sejak 12 Januari 2019</td>
+					</tr>
+				</table>
+				<button type="button" class="btn btn-success m-2">kirim Pesan</button>
+				<button type="button" class="btn btn-success m-2">Ikuti</button>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+        <div class="col-sm-6">
+            <div class="barang-jual ml-5 p-1 mt-3 shadow mb-3 bg-white rounded">
+                <img class="gambar-jual img-thumbnail rounded" src="../img/telur.jpg">
+                <h5>Telur Jakarta</h5>
+                <h6 class="text-success font-weight-bold">RP 50.0000</h6>
+                <button type="button" class="btn btn-success btn-lg btn-block">Beli Sekarang</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
